33aaf78e95c075ca92ed0833d932e0769a7bf142
talithafawwaz <talithafawwaz07@gmail.com>
2023-05-11 17:37:59 +0700

    enhance server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const PORT = process.env.PORT || 9000
 http
   .createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello Cloud Run')
+    res.end('Hello ini branch dev')
   })
   .listen(PORT, () => console.log('Listening on', PORT))
